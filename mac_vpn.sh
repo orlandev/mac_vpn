040b0e1053c697d02c77186f6e29ed30a0ac180c
orlandev <onovas05@gmail.com>
2023-04-30 12:01:09 -0400

    Add param validation

--- mac_vpn.sh
+++ mac_vpn.sh
@@ -1,13 +1,22 @@
 
-MACCHANGER="macchangeer"
-BREW="macchangeer"
+MACCHANGER="macchanger"
 
 if ! command -v ${MACCHANGER} >/dev/null; then
   echo "This script requires ${MACCHANGER} to be installed and on your PATH ..."
   echo "Install ${MACCHANGER} using:"
   echo "- brew update"
   echo "- brew install acrogenesis/macchanger/macchanger"
-  if ! command -v ${MACCHANGER} >/dev/null; then
-
   exit 1
 fi
+
+echo "[ OK ] ${MACCHANGER} INSTALLED"
+echo ""
+
+if [ -z "$1" ]
+  then
+    echo "MacVPN - Changer - For Inmersoft Network"
+fi
+
+
+
+
